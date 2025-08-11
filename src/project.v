127be097c79a216c562e3b5372d3b83c6e42e68c
wandouuu <130308899+wandouuu@users.noreply.github.com>
2025-08-10 21:55:26 -0400

    Edit module for setup

--- src/project.v
+++ src/project.v
@@ -39,6 +39,6 @@ module tt_um_uwasic_onboarding_brendan_leung (
   );
 
   // List all unused inputs to prevent warnings
-  wire _unused = &{ena, clk, rst_n, 1'b0, ui_in[7:3], uio_in};
+  wire _unused = &{ena, ui_in[7:3], uio_in, 1'b0};
 
 endmodule
