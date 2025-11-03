ba0fb80825e13aa3e7f09b6bc1b359add2d87414
wandouuu <130308899+wandouuu@users.noreply.github.com>
2025-11-02 21:12:42 -0500

    Config details

--- src/project.v
+++ src/project.v
@@ -5,7 +5,16 @@
 
 `default_nettype none
 
-module tt_um_example (
+module tt_um_uwasic_brendan_leung (
+    
+    assign uio_oe = 8'hFF;
+
+    wire [7:0] en_reg_out_7_0;
+    wire [7:0] en_reg_out_15_8;
+    wire [7:0] en_reg_pwm_7_0;
+    wire [7:0] en_reg_pwm_15_8;
+    wire [7:0] pwm_duty_cycle;
+    
     input  wire [7:0] ui_in,    // Dedicated inputs
     output wire [7:0] uo_out,   // Dedicated outputs
     input  wire [7:0] uio_in,   // IOs: Input path
@@ -14,6 +23,17 @@ module tt_um_example (
     input  wire       ena,      // always 1 when the design is powered, so you can ignore it
     input  wire       clk,      // clock
     input  wire       rst_n     // reset_n - low to reset
+
+    pwm_peripheral pwm_peripheral_inst (
+    .clk(clk),
+    .rst_n(rst_n),
+    .en_reg_out_7_0(en_reg_out_7_0),
+    .en_reg_out_15_8(en_reg_out_15_8),
+    .en_reg_pwm_7_0(en_reg_pwm_7_0),
+    .en_reg_pwm_15_8(en_reg_pwm_15_8),
+    .pwm_duty_cycle(pwm_duty_cycle),
+    .out({uio_out, uo_out})
+  );
 );
 
   // All output pins must be assigned. If not used, assign to 0.
@@ -22,6 +42,6 @@ module tt_um_example (
   assign uio_oe  = 0;
 
   // List all unused inputs to prevent warnings
-  wire _unused = &{ena, clk, rst_n, 1'b0};
+  wire _unused = &{ena, clk, rst_n, ui_in[7:3], uio_in, 1'b0};
 
 endmodule
