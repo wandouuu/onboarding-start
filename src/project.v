526e4d14ab043901afb193e43adcf355d66d95af
wandouuu <130308899+wandouuu@users.noreply.github.com>
2025-11-03 22:52:10 -0500

    Edit config

--- src/project.v
+++ src/project.v
@@ -6,15 +6,7 @@
 `default_nettype none
 
 module tt_um_uwasic_onboarding_wandouuu (
-    
-    assign uio_oe = 8'hFF;
 
-    wire [7:0] en_reg_out_7_0;
-    wire [7:0] en_reg_out_15_8;
-    wire [7:0] en_reg_pwm_7_0;
-    wire [7:0] en_reg_pwm_15_8;
-    wire [7:0] pwm_duty_cycle;
-    
     input  wire [7:0] ui_in,    // Dedicated inputs
     output wire [7:0] uo_out,   // Dedicated outputs
     input  wire [7:0] uio_in,   // IOs: Input path
@@ -24,7 +16,18 @@ module tt_um_uwasic_onboarding_wandouuu (
     input  wire       clk,      // clock
     input  wire       rst_n     // reset_n - low to reset
 
-    pwm_peripheral pwm_peripheral_inst (
+  
+);
+
+  assign uio_oe = 8'hFF;
+
+  wire [7:0] en_reg_out_7_0;
+  wire [7:0] en_reg_out_15_8;
+  wire [7:0] en_reg_pwm_7_0;
+  wire [7:0] en_reg_pwm_15_8;
+  wire [7:0] pwm_duty_cycle;
+
+  pwm_peripheral pwm_peripheral_inst (
     .clk(clk),
     .rst_n(rst_n),
     .en_reg_out_7_0(en_reg_out_7_0),
@@ -34,14 +37,22 @@ module tt_um_uwasic_onboarding_wandouuu (
     .pwm_duty_cycle(pwm_duty_cycle),
     .out({uio_out, uo_out})
   );
-);
 
-  // All output pins must be assigned. If not used, assign to 0.
-  assign uo_out  = ui_in + uio_in;  // Example: ou_out is the sum of ui_in and uio_in
-  assign uio_out = 0;
-  assign uio_oe  = 0;
+  spi_peripheral spi_peripheral_inst (
+    .clk(clk),
+    .rst_n(rst_n),
+    .COPI(ui_in[1]),
+    .nCS(ui_in[2]),
+    .SCLK(ui_in[0]),
+    .en_reg_out_7_0(en_reg_out_7_0),
+    .en_reg_out_15_8(en_reg_out_15_8),
+    .en_reg_pwm_15_8(en_reg_pwm_15_8),
+    .en_reg_pwm_7_0(en_reg_pwm_7_0),
+    .pwm_duty_cycle(pwm_duty_cycle)
+
+  );
 
   // List all unused inputs to prevent warnings
-  wire _unused = &{ena, clk, rst_n, ui_in[7:3], uio_in, 1'b0};
+  wire _unused = &{ena, ui_in[7:3], uio_in, 1'b0};
 
 endmodule
--- src/spi_peripheral.v
+++ src/spi_peripheral.v
@@ -2,13 +2,21 @@
 
 module spi_peripheral(
     
-    // Need input
+    input wire clk,
+    input wire rst_n,
+
+    // input
+    input wire COPI, // ui_in[1]
+    input wire nCS, // ui_in[2]
+    input wire SCLK, // ui_in[0]
 
     // Output registers
     output reg [7:0] en_reg_out_7_0,
-    output reg [15:8] en_reg_out_15_8,
+    output reg [7:0] en_reg_out_15_8,
     output reg [7:0] en_reg_pwm_7_0,
-    output reg [15:8] en_reg_pwm_15_8,
+    output reg [7:0] en_reg_pwm_15_8,
     output reg {7:0} pwm_duty_cycle
 
 );
+
+endmodule
